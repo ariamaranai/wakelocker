8326ce6840203db4d5b219df0cb66faa5260d90e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-25 13:43:57 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,19 +1,17 @@
-chrome.action.onClicked.addListener(() =>
-  chrome.runtime.getContexts({}, contexts =>
-    chrome.action.setIcon({
-      path: contexts.length < 2
-        ? (
-          chrome.offscreen.createDocument({
-            justification: "",
-            reasons: ["BLOBS"],
-            url: "offscreen.htm"
-          }),
-          "on.png"
-        )
-        : (
-          chrome.offscreen.closeDocument(),
-          "off.png"
-        )
-    })
-  )
+chrome.action.onClicked.addListener(async () =>
+  chrome.action.setIcon({
+    path: (await chrome.runtime.getContexts({})).length < 2
+      ? (
+        chrome.offscreen.createDocument({
+          justification: "",
+          reasons: ["BLOBS"],
+          url: "offscreen.htm"
+        }),
+        "on.png"
+      )
+      : (
+        chrome.offscreen.closeDocument(),
+        "off.png"
+      )
+  })
 );
